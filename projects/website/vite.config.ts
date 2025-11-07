eb4ccda8393630a7d1de9301e8cd6988f5106bf5
Kristl Tyler <ktyler@aerospike.com>
2025-11-07 13:54:31 -0800

    Update vite.config.ts

--- projects/website/vite.config.ts
+++ projects/website/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api' : {
-				target: 'http://localhost:8082',
+				target: 'http://localhost:7082',
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			}
 		}
