2aad966a07475676f657e98248bd7833e7ad510c
Kristl Tyler <ktyler@aerospike.com>
2025-11-14 13:31:56 -0800

    Update index.tsx
    guessing about the product list instead of product

--- projects/website/src/pages/Product/index.tsx
+++ projects/website/src/pages/Product/index.tsx
@@ -39,7 +39,7 @@ export const loader = async (product: string) => {
         statusText: "Not Found"
     });
 
-    let related: Products[] =[]
+    let related: ProductList[] =[]
     if (Array.isArray(data.related) && data.related.length > 0) {
         const ids = data.related.join(","); // e.g. "1025,1033"
         const relRes = await fetch(`/api/products/related/${ids}`);
@@ -54,7 +54,7 @@ export const loader = async (product: string) => {
 
 interface LoaderProps {
     product: Product
-    related: Product[]
+    related: ProductList[]
 }
 
 const Product = () => {
